4ba1281e5ba11a4ce86c8e31c8b804c65d510e72
Sergio Daniel Lozano <zheref@icloud.com>
2017-07-13 20:46:22 -0500

    Initial Commit

---
+++ L5Player/L5Player.h
@@ -0,0 +1,19 @@
+//
+//  L5Player.h
+//  L5Player
+//
+//  Created by Sergio Daniel on 7/13/17.
+//  Copyright © 2017 Li5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for L5Player.
+FOUNDATION_EXPORT double L5PlayerVersionNumber;
+
+//! Project version string for L5Player.
+FOUNDATION_EXPORT const unsigned char L5PlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <L5Player/PublicHeader.h>
+
+
---
+++ L5PlayerTests/L5PlayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  L5PlayerTests.swift
+//  L5PlayerTests
+//
+//  Created by Sergio Daniel on 7/13/17.
+//  Copyright © 2017 Li5. All rights reserved.
+//
+
+import XCTest
+@testable import L5Player
+
+class L5PlayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
